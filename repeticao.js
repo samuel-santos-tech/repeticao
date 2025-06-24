8123ff74f40f523cca38ce94b1b6285572b6d5a7
unknown <samuelnascimento6282@digitalcollege.internal>
2025-06-24 09:31:26 -0300

    repeticao

---
+++ repeticao.js
@@ -0,0 +1,48 @@
+// for (let i = 0; i <= 5; i++) {
+//     console.log(`número ${i}`);
+// }
+
+// console.log('fim do loop');
+
+// const marcasDeCarro = ['Honda', 'BYD', 'Toyota', 'Jeep'];
+
+// const itensDoArray = marcasDeCarro.length;
+
+// const frase = 'Samuel Santos';
+
+// for (let i = 0; i < marcasDeCarro.length; i++) {
+//     console.log(marcasDeCarro[i]);
+// }
+
+
+// const frutas = ['banana', 'morango', 'laranja', 'uva']
+
+//     for (let i = 0; i < frutas.length; i++) {
+//   if (frutas[i] === 'laranja') {
+//     frutas[i] = 'maçã';
+//   }
+// }
+
+// console.log(frutas);
+
+const tenis = [
+    {
+        marca: 'Nike',
+        modelo: 'Air Max',
+        cor: 'Preto',
+        tamanho: [42, 43, 44],
+        preco: 499.99,
+        estoque: 10
+    },
+    {
+        marca: 'Adidas',
+        modelos: 'Ultraboost',
+        cor: 'branco',
+        tamanho: [40, 41, 42],
+        preco: 599.99,
+        estoque: 5,
+    },
+    {
+        
+    }
+]
