f69c4c79af06de8f5797b7732fc12588bfb0694e
unknown <samuelnascimento6282@digitalcollege.internal>
2025-07-01 09:56:02 -0300

    arrays

---
+++ arrays-metodos.js
@@ -0,0 +1,40 @@
+// const frutas = ['banana', 'morango', 'laranja', 'uva']
+
+// frutas.unshift('morango');
+// console.log(frutas);
+
+// const letras =  ['A', 'B', 'C', 'D', 'E', 'F'];
+
+// letras.splice(2,2,'X','Z');
+// console.log(letras);
+
+// const novasLetras = letras.slice(2, 5);
+// console.log(novasLetras);
+
+// const listNumeros = [1, 2 , 3, 4, 5, 6 , 7];
+
+// const novosNumeros = listNumeros.map((item)=>{
+//  if(item % 2 === 0) {
+//     return item * 2;
+//  }
+
+//  return item;
+// });
+
+
+// novosNumeros.forEach((item) => {
+//     console.log(item)
+// });
+
+// const listaDeNumeros = [10, 20, 30 , 40, 50, 60]
+
+// const encontrado = listaDeNumeros.find(item) => {
+//     return item > 45;
+// }
+
+// console.log(encontrado);
+
+const nomes = ['Lucas', 'Rithely', 'Samuel', 'Tadeu']
+nomes.splice(0,1,)
+nomes.splice(2,0,'Magna')
+console.log(nomes)
